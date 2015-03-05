ecde4636d2c517cc1763211d760b72bea7985e74
jenglezou <jenglezou@aol.com>
2015-03-05 16:55:24 +0000

    Initial commit.

---
+++ helpers/URLS.js
@@ -0,0 +1,14 @@
+var Urls = (function () {
+    function Urls() {}
+    Urls.prototype.load = function (dict) {
+        dict.set('SYSTEST', 'http://www.google.com');
+        dict.set('LIVE', 'http://www.google.com');
+        dict.set('UAT', 'http://www.google.com');
+        dict.set('LIVE', 'http://www.google.com');
+        dict.set('LOCAL', 'http://localhost');
+        dict.set('LOCALHOST', 'http://localhost');
+    };
+    return Urls;
+})();
+module.exports = Urls;
+
