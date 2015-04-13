2fb855acb8baae75fc7d3317129e0badd1280592
jenglezou <jenglezou@aol.com>
2015-04-13 08:43:26 +0100

    example spec

--- tests/Example.pro.js
+++ tests/Example.pro.js
@@ -1,58 +1,61 @@
-var Automation = require("../../helpers/automation.js");
-var automation = new Automation();
-var key = require('keyword'); // keyword functionality
-var Dictionary = require('dictionaryjs');
+describe('Example test scenario', function () {				
+// ---------------------------------------------------  // ---------------------------------------------------
+    it("Start", function () { 							     it("DoSomething C", function () {                 
+		var browser="chrome";                                   var param1='data1';                            
+		var url='http://www.Example.com';                       var param2='data2';                            
+        // code for functionality goes here                     // code for functionality goes here            
+        // can use expect().toEqual() for assertions            // can use expect().toEqual() for assertions   
+    },120000);                                               },120000);                                        
+// ---------------------------------------------------  // ---------------------------------------------------  
+    it("Signin", function () {                          	it("DoSomething D", function () {                     
+        var username='testuser1';                               var param1='data1';                            
+		var password='3X4mpl3';                                 var param2='data2';                            
+        // code for functionality goes here                     // code for functionality goes here            
+        // can use expect().toEqual() for assertions            // can use expect().toEqual() for assertions   
+    },120000);                                          	},120000); 
+// ---------------------------------------------------  // ---------------------------------------------------    
+    it("DoSomething A", function () {                   	it("DoSomething E", function () {  
+        var param1='data1';                                     var param1='data1';                            
+        var param2='data2';                                     var param2='data2';                            
+        // code for functionality goes here             		// code for functionality goes here
+        // can use expect().toEqual() for assertions    		// can use expect().toEqual() for assertions 
+    },120000);                                          	},120000); 
+// ---------------------------------------------------  // --------------------------------------------------- 
+    it("DoSomething B", function () {                   	it("Signout", function () { 
+        var param1='data1';                            
+        var param2='data2';                            
+        // code for functionality goes here              		// code for functionality goes here 
+        // can use expect().toEqual() for assertions    		// can use expect().toEqual() for assertions 
+    },120000);                                          	},120000);
+                                                        // ---------------------------------------------------
+                                                        	it("End", function () {
+                                                        		// code for functionality goes here
+                                                        		// can use expect().toEqual() for assertions
+                                                        	},120000);   
+                                                        }, 60000);   
+                                                          
+                                                          
+                                                          
+                                                          
+                                                          
 
-var dictData = new Dictionary();
-try {
-    var TestData = require("../../data/TestData/" + browser.params.testEnv + ".Example.data.js");
-    var testData = new TestData();
-    testData.load(dictData);
-} catch(err) {
-    console.log(err)
-}
 
-var dictURLS = new Dictionary();
-var Urls = require("../../helpers/URLS.js");
-var urls = new Urls();
-urls.load(dictURLS);
 
-// -----------------------------------------------------------------------------
-describe('Example.Session1', function () {
-    var iStep = 1;
 
-    browser.get(dictURLS.get(browser.params.testEnv.toUpperCase()));
 
-    /*
-    // Trying this to jump out after a failed it/step
-    afterEach(function() {
-        if (!this.results().passed()) {
-                return false;
-        }
-    });
-    */
 
-// -----------------------------------------------------------------------------
-    automation.using("running Session1 test script", require('./Session1.script.js'),
-        function (step) {
-            var sParams = "";
-            var sObject = step.action.substring(0, step.action.indexOf('.'));
-            for (var property in (step.params)[0]) {
-                if ((step.params)[0].hasOwnProperty(property)) {
-                    (step.params)[0][property] = automation.resolveParams(dictData, (step.params)[0][property]);
-                    sParams = sParams + property + '=' + (step.params)[0][property] + ',';
-                }
-            }
 
-            it("Step " + (('0' + iStep++).slice(-2)) + ": Keyword=" + step.action + ", Data=[" + sParams + "]", function () {
-                if (step.action.slice(0,1) != '#') {
-                    //Load keyword file (name must be in the form 'object.keywords.js'). "try" handles exception when loading same object.keywords.js file
-                    try { key(require('../../keywords/' + sObject + '.keywords.js')); } catch(err) {
-                        console.log(err)
-                    }
-                    key.run(step.action, step.params).then(function () { });
-                }
-            }, 600000);
-        });
 
-}, 600000);
+
+
+
+
+
+
+
+
+
+
+
+
+
